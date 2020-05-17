a89dc5f6aa89043d531998191c396c1a92a2a271
manu-sh <s3gmentationfault@gmail.com>
2020-05-17 23:43:33 +0200

    uff

--- dht_crawler.cpp
+++ dht_crawler.cpp
@@ -16,27 +16,26 @@ dht_crawler::dht_crawler(std::string result_file, int session_num, int start_por
 	m_result_file = result_file;
 }
 
-void dht_crawler::print_settings() const 
-{
-	cout << "\tupload_rate_limit: " << m_upload_rate_limit << endl;
-	cout << "\tdownload_rate_limit: " << m_download_rate_limit << endl;
-	cout << "\tactive_downloads: " << m_active_downloads << endl;
-	cout << "\talert_queue_size: " << m_alert_queue_size << endl;
-	cout << "\tdht_announce_interval: " << m_dht_announce_interval << endl;
-	cout << "\ttorrent_upload_limit: " << m_torrent_upload_limit << endl;
+void dht_crawler::print_settings() const {
+
+	cout << "\tupload_rate_limit: "      << m_upload_rate_limit      << endl;
+	cout << "\tdownload_rate_limit: "    << m_download_rate_limit    << endl;
+	cout << "\tactive_downloads: "       << m_active_downloads       << endl;
+	cout << "\talert_queue_size: "       << m_alert_queue_size       << endl;
+	cout << "\tdht_announce_interval: "  << m_dht_announce_interval  << endl;
+	cout << "\ttorrent_upload_limit: "   << m_torrent_upload_limit   << endl;
 	cout << "\ttorrent_download_limit: " << m_torrent_download_limit << endl;
-	cout << "\tauto_manage_startup: " << m_auto_manage_startup << endl;
-	cout << "\tauto_manage_interval: " << m_auto_manage_interval << endl;
-	cout << "\tstart_port: " << m_start_port << endl;
-	cout << "\tsession_num: " << m_session_num << endl;
-	cout << "\ttotal_intervals: " << m_total_intervals << endl;
-	cout << "\twriting_interval: " << m_writing_interval << endl;
-	cout << "\tresult_file: " << m_result_file << endl;
+	cout << "\tauto_manage_startup: "    << m_auto_manage_startup    << endl;
+	cout << "\tauto_manage_interval: "   << m_auto_manage_interval   << endl;
+	cout << "\tstart_port: "             << m_start_port             << endl;
+	cout << "\tsession_num: "            << m_session_num            << endl;
+	cout << "\ttotal_intervals: "        << m_total_intervals        << endl;
+	cout << "\twriting_interval: "       << m_writing_interval       << endl;
+	cout << "\tresult_file: "            << m_result_file            << endl;
 	cout << "\ttrackers:" << endl;
 
 	for (const auto &p : m_trackers)
 		cout << "\t\t" << p.first << ":" << p.second << endl;
-
 }
 
 void dht_crawler::run() {
@@ -44,8 +43,7 @@ void dht_crawler::run() {
 	cout << "Starting running with" << endl;
 	this->print_settings();
 
-	int intervals = 0;
-	while (intervals < m_total_intervals) {
+	for (int intervals = 0; intervals < m_total_intervals;) {
 
 		for (unsigned i = 0; i < m_sessions.size(); ++i) {
 			std::vector<lt::alert*> alerts;
@@ -54,9 +52,8 @@ void dht_crawler::run() {
 			this->handle_alerts(m_sessions.at(i), &alerts);
 		}
 
-		++intervals;
 
-		if (intervals % m_writing_interval == 0) {
+		if (++intervals % m_writing_interval == 0) {
 
 			cout << "interval " << intervals << " done, writing result file...";
 			if (this->write_result_file())
@@ -78,144 +75,130 @@ void dht_crawler::run() {
 		cout << " failed." << endl;
 
 	// TODO: bug sure
-	for (unsigned int i = 0; i < m_sessions.size(); ++i)
-	{
+	for (unsigned i = 0; i < m_sessions.size(); ++i) {
+
 		auto torrents = m_sessions[i]->get_torrents();
-		for (unsigned int j = 0; j < torrents.size(); ++j)
-		{
+		for (unsigned j = 0; j < torrents.size(); ++j)
 			m_sessions[i]->remove_torrent(torrents[j]);
-		}
+
 		// delete m_sessions[i]; 
 	}
 }
 
-void dht_crawler::handle_alerts(libtorrent::session* psession, std::vector<libtorrent::alert*>* palerts)
-{
-	while (palerts->size() > 0)
-	{
+void dht_crawler::handle_alerts(libtorrent::session* psession, std::vector<libtorrent::alert*>* palerts) {
+
+	while (palerts->size() > 0) {
+
 		auto palert = palerts->front();
 		palerts->pop_back();
 		std::string info_hash;
 
-		switch (palert->type())
-		{
-		case libtorrent::add_torrent_alert::alert_type:
-		{
-			auto p1 = (libtorrent::add_torrent_alert*) palert;
-			p1->handle.set_upload_limit(m_torrent_upload_limit);
-			p1->handle.set_download_limit(m_torrent_download_limit);
-			break;
-		}
-		case libtorrent::dht_announce_alert::alert_type:
-		{
-			auto p2 = (libtorrent::dht_announce_alert*) palert;
-			info_hash = p2->info_hash.to_string();
+		switch (palert->type()) {
 
-			// update meta
-			// c++ map: if meta[info_hash] does not exist,
-			// it will be inserted and initialized to zero
-			if (m_meta[info_hash] > 0)
-			{
-				++m_meta[info_hash];
-			}
-			else
-			{
-				m_meta[info_hash] = 1;
-				++m_current_meta_count;
-			}
-			break;
-		}
-		case libtorrent::dht_get_peers_alert::alert_type:
-		{
-			auto p3 = (libtorrent::dht_get_peers_alert*) palert;
-			info_hash = p3->info_hash.to_string();
-			if (m_meta[info_hash] > 0)
-			{
-				++m_meta[info_hash];
-			}
-			else
-			{
-				m_info_hash_from_getpeers.push_back(info_hash);
-				m_meta[info_hash] = 1;
-				++m_current_meta_count;
-			}
-			break;
-		}
-		default:
-			break;
+			case libtorrent::add_torrent_alert::alert_type:
+				{
+					auto p1 = (libtorrent::add_torrent_alert*) palert;
+					p1->handle.set_upload_limit(m_torrent_upload_limit);
+					p1->handle.set_download_limit(m_torrent_download_limit);
+					break;
+				}
+			case libtorrent::dht_announce_alert::alert_type:
+				{
+					auto p2 = (libtorrent::dht_announce_alert*) palert;
+					info_hash = p2->info_hash.to_string();
+
+					// update meta
+					// c++ map: if meta[info_hash] does not exist,
+					// it will be inserted and initialized to zero
+					if (m_meta[info_hash] > 0)
+					{
+						++m_meta[info_hash];
+					}
+					else
+					{
+						m_meta[info_hash] = 1;
+						++m_current_meta_count;
+					}
+					break;
+				}
+			case libtorrent::dht_get_peers_alert::alert_type:
+				{
+					auto p3 = (libtorrent::dht_get_peers_alert*) palert;
+					info_hash = p3->info_hash.to_string();
+					if (m_meta[info_hash] > 0)
+					{
+						++m_meta[info_hash];
+					}
+					else
+					{
+						m_info_hash_from_getpeers.push_back(info_hash);
+						m_meta[info_hash] = 1;
+						++m_current_meta_count;
+					}
+					break;
+				}
+			default:
+				break;
 		}
 
 		// delete palert;
 	}
 }
 
-void dht_crawler::create_sessions()
-{
-	int& start_port = m_start_port;
+void dht_crawler::create_sessions() {
 
-	for (int i = 0; i < m_session_num; ++i)
-	{
+	int &start_port = m_start_port;
+
+	for (int i = 0; i < m_session_num; ++i) {
+
 		auto psession = new libtorrent::session;
-		//psession->set_alert_mask(libtorrent::alert::category_t::all_categories);
+		// psession->set_alert_mask(libtorrent::alert::category_t::all_categories);
+
 		lt::error_code ec;
 		psession->listen_on(std::make_pair(start_port + i, start_port + i), ec);
 
-		for (int j = 0; j < m_trackers.size(); ++j)
-		{
+		for (unsigned j = 0; j < m_trackers.size(); ++j)
 			psession->add_dht_router(m_trackers[j]);
-		}
 
 		auto settings = psession->get_settings();
 		/*
-		settings.upload_rate_limit = m_upload_rate_limit;
-		settings.download_rate_limit = m_download_rate_limit;
-		settings.active_downloads = m_active_downloads;
-		settings.auto_manage_interval = m_auto_manage_interval;
-		settings.auto_manage_startup = m_auto_manage_startup;
-		settings.dht_announce_interval = m_dht_announce_interval;
-		settings.alert_queue_size = m_alert_queue_size;
+		   settings.upload_rate_limit = m_upload_rate_limit;
+		   settings.download_rate_limit = m_download_rate_limit;
+		   settings.active_downloads = m_active_downloads;
+		   settings.auto_manage_interval = m_auto_manage_interval;
+		   settings.auto_manage_startup = m_auto_manage_startup;
+		   settings.dht_announce_interval = m_dht_announce_interval;
+		   settings.alert_queue_size = m_alert_queue_size;
+		   psession->set_settings(settings);
 		*/
-		//psession->set_settings(settings);
 		m_sessions.push_back(psession);
+
 	}
 }
 
-void dht_crawler::add_magnet(std::string link)
-{
+bool dht_crawler::write_result_file() {
 
-}
-
-bool dht_crawler::write_result_file()
-{
 	std::fstream fs;
-	fs.open(m_result_file, std::fstream::out | std::fstream::trunc);
+	fs.open(m_result_file, std::fstream::out|std::fstream::trunc);
 
-	if (fs.is_open())
-	{
-		for (auto iter = m_meta.begin(); iter != m_meta.end(); ++iter)
-		{
-			const std::string& s = iter->first;
-			for (unsigned int i = 0; i < s.size(); ++i)
-			{
-				fs << std::hex;
-				unsigned char c = s[i];
-				if (c < 16)
-				{
-					fs << '0' << (unsigned int)c;
-				}
-				else
-				{
-					fs << (unsigned int)c;
-				}
-			}
-			fs << std::dec;
-			fs << '\t' << iter->second << endl;
-		}
-		fs.close();
-		return true;
-	}
-	else
-	{
+	if (!fs.is_open())
 		return false;
+
+	for (auto iter = m_meta.begin(); iter != m_meta.end(); ++iter) {
+
+		const std::string& s = iter->first;
+		for (unsigned int i = 0; i < s.size(); ++i) {
+
+			fs << std::hex;
+			if (unsigned char c = s[i]; c < 16)
+				fs << '0' << (unsigned int)c;
+			else
+				fs << (unsigned int)c;
+		}
+
+		fs << std::dec;
+		fs << '\t' << iter->second << endl;
 	}
+
+	return true;
 }
--- dht_crawler.h
+++ dht_crawler.h
@@ -1,8 +1,6 @@
 // define BOOST_ASIO_SEPARATE_COMPILATION
 
-#ifndef _DHT_CRAWLER_H
-#define _DHT_CRAWLER_H
-
+#pragma once
 #include <libtorrent/entry.hpp>
 #include <libtorrent/bencode.hpp>
 #include <libtorrent/session.hpp>
@@ -11,52 +9,50 @@
 #include <vector>
 #include <map>
 
-class dht_crawler
-{
-public:
+class dht_crawler {
 
-	dht_crawler(std::string result_file, int session_num, int start_port, int total_intervals);
-	void create_sessions();
-	void print_settings() const;
-	void run();
+	public:
 
-	// session settings
-	int m_upload_rate_limit = 200000;
-	int m_download_rate_limit = 200000;
-	int m_active_downloads = 30;
-	int m_alert_queue_size = 4000;
-	int m_dht_announce_interval = 60;
-	int m_torrent_upload_limit = 20000;
-	int m_torrent_download_limit = 20000;
-	int m_auto_manage_startup = 30;
-	int m_auto_manage_interval = 15;
+		dht_crawler(std::string result_file, int session_num, int start_port, int total_intervals);
+		void create_sessions();
+		void print_settings() const;
+		void run();
 
-	// crawler settings
-	int m_start_port = 32900;
-	int m_session_num = 50;
-	int m_total_intervals = 60;
-	int m_writing_interval = 60;
-	std::string m_result_file;
+		// session settings
+		int m_upload_rate_limit = 200000;
+		int m_download_rate_limit = 200000;
+		int m_active_downloads = 30;
+		int m_alert_queue_size = 4000;
+		int m_dht_announce_interval = 60;
+		int m_torrent_upload_limit = 20000;
+		int m_torrent_download_limit = 20000;
+		int m_auto_manage_startup = 30;
+		int m_auto_manage_interval = 15;
 
-	// example: router.bittorrent.com
-	std::vector< std::pair<std::string, int> > m_trackers = {
-		{"router.bittorrent.com",  6881},
-		{"router.utorrent.com",    6881},
-		{"router.bitcomet.com",    6881},
-		{"dht.transmissionbt.com", 6881}
-	};
+		// crawler settings
+		int m_start_port = 32900;
+		int m_session_num = 50;
+		int m_total_intervals = 60;
+		int m_writing_interval = 60;
+		std::string m_result_file;
 
-private:
+		// example: router.bittorrent.com
+		std::vector< std::pair<std::string, int> > m_trackers = {
+			{"router.bittorrent.com",  6881},
+			{"router.utorrent.com",    6881},
+			{"router.bitcomet.com",    6881},
+			{"dht.transmissionbt.com", 6881}
+		};
 
-	void handle_alerts(libtorrent::session*, std::vector<libtorrent::alert*>*);
-	void add_magnet(std::string link);
-	bool write_result_file();
+	private:
 
-	// storage
-	int m_current_meta_count = 0;
-	std::vector<libtorrent::session*> m_sessions;
-	std::vector<std::string> m_info_hash_from_getpeers;
-	std::map<std::string, int> m_meta;
+		void handle_alerts(libtorrent::session*, std::vector<libtorrent::alert*>*);
+		// void add_magnet(std::string link);
+		bool write_result_file();
+
+		// storage
+		int m_current_meta_count = 0;
+		std::vector<libtorrent::session*> m_sessions;
+		std::vector<std::string> m_info_hash_from_getpeers;
+		std::map<std::string, int> m_meta;
 };
-
-#endif // _DHT_CRAWLER_H
